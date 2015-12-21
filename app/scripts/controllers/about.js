8581aad99a01e29a6accdaa4718fc67073060e6a
micbecke <mike@wiseagent.com>
2015-12-21 12:15:46 -0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wheelhoundsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wheelhoundsApp
+ */
+angular.module('wheelhoundsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
